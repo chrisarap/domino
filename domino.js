4450c65926b8c7d000b92b6fabdc976bcd61cd96
chrisarap <carteaga460@gmail.com>
2021-07-08 22:46:04 -0400

    28 pieces created

---
+++ domino.js
@@ -0,0 +1,23 @@
+class Domino {
+
+	create_array(){
+		let arr = [];
+
+		for (var i = 0; i <= 6; i++) {
+			for (var j = 0; j <= 6; j++) {
+
+				var flag = 0;
+
+				arr.filter(piece => piece[0] == Math.min(i, j) && piece[1] == Math.max(i, j) ? flag++ : true);
+
+				if (flag == 0 ) {
+					arr.push([Math.min(i, j), Math.max(i, j)]);
+				}
+			} 
+		}
+		return arr;		
+	}
+}
+
+var test = new Domino();
+console.log(test.create_array());
